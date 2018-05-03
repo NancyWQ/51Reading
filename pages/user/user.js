67217506b6df20a270583b6209fffe4d0e0cc8c6
NancyWQ <bb08280703@163.com>
2018-05-03 22:17:25 +0800

    5.3e

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,10 +1,12 @@
+const app=getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    userInfo:{},
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
@@ -18,14 +20,40 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    console.log(app.globalData.userInfo);
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+          })
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    console.log('onshow');
+   
   },
 
   /**
