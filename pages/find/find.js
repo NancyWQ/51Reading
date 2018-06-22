8002076d08d8a3dd5f15b70e45a9f76b32df9a56
NancyWQ <bb08280703@163.com>
2018-06-22 20:08:04 +0800

    6.22

--- pages/find/find.js
+++ pages/find/find.js
@@ -4,9 +4,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    readyRead:true,
+    willRead:true,
+    book: { id: "1", title: "三国演义", doubanPoint: 9.4, img: "../../img/index/81.jpg", imgIcon:"../../img/icon/star.png", remarkNum: 7594, bookSeeing: ["◎《三国演义》是根据三国时期的史实和民间传说创作而成的优秀历史小说，它的内容丰富多彩，为读者留下了深厚而多彩的认识价值", "◎《三国演义》是根据三国时期的史实和民间传说创作而成的优秀历史小说，它的内容丰富多彩，为读者留下了深厚而多彩的认识价值", "◎《三国演义》是根据三国时期的史实和民间传说创作而成的优秀历史小说，它的内容丰富多彩，为读者留下了深厚而多彩的认识价值"], bookContent: ["《三国演义》是根据三国时期的史实和民间传说创作而成的优秀历史小说，它的内容丰富多彩，为读者留下了深厚而多彩的认识价值"], bookAuthor: ["《三国演义》是根据三国时期的史实和民间传说创作而成的优秀历史小说，它的内容丰富多彩，为读者留下了深厚而多彩的认识价值"]},
+  userReadInfo:{userId:1,read:0,remarkGood:0,remarkOrdinary:0,want:0,noFeeling:0,noRemark:1164},
   },
-
+  choose:function(){
+    let _this=this;
+    let aleadyRead=_this.data.readyRead;
+    this.setData({readyRead:!aleadyRead});
+    //往后台发送数据
+  },
+  choose1:function(){
+    let _this = this;
+    let willRead = _this.data.willRead;
+    this.setData({willRead: !willRead});
+    //往后台发送数据
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    indexPopular: {total:254,pageNow:1,pageCount:9,content:[{ id: "1", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }, { id: "2", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }, { id: "3", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }, { id: "4", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }, { id: "5", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }, { id: "6", title: "这九本书帮你掌控情绪，变得平静", img: "../../img/index/81.jpg", date: "01-10" }],}
+  },
+  nextPage:function(){
+    // console.log(this.data.indexPopular.pageNow);
+    if (this.data.indexPopular.pageNow < this.data.indexPopular.pageCount){
+      // 发送请求，获取数据，最后设置成为新的内容
+    } else if (this.data.indexPopular.pageNow=this.data.indexPopular.pageCount){
+      console.log("这已经是最后一页了")
+    }
   },
 
   /**
